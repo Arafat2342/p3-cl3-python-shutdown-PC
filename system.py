663fde8834052b240159a9fc4bb5ad7c8514816e
Arafat2342 <jashoresinger@gmail.com>
2025-04-18 19:41:28 +0600

    shutdown

--- system.py
+++ system.py
@@ -4,5 +4,7 @@ shutdown = input('Do you wish to shutdown your PC? (Yes / No): ')
 
 if shutdown.lower() == 'no':
     exit()
-else:
+elif shutdown.lower() == 'yes':
     os.system("shutdown /s /t 1")
+else:
+    print('invalid input!')
